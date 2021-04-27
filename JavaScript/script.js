8e6f17be15c7399f5c34175f8891ab396d04465d
Jon-Ledo <jonledo.code@gmail.com>
2021-04-27 01:08:15 -0400

    added mofal window JS, styles need work. Changed heading style

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,76 +1,20 @@
 'use strict';
 
+// MODAL WINDOW
 // variables
-const formElement = document.querySelector('form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const textInput = document.querySelector('#message');
-// Create parent container variable to append new items to
-const parentContainer = document.querySelector('.comment-section');
+const modal = document.querySelector('.modal')
+const modalBtnSelector = document.querySelectorAll('.btn');
+const modalOpenBtn = modalBtnSelector[2];
+const modalCloseBtn = document.querySelector('.modal-close');
+const all = document.querySelector('html');
 
-// Current date variables
-let currentDate = new Date();
-let numericalDate = currentDate.getDate();
-let currentDay = currentDate.getDay();
-let currentMonth = currentDate.getMonth();
-let currentYear = currentDate.getFullYear();
+modalOpenBtn.addEventListener('click', function() {
+    // Removes hidden class, REVEAL the modal window
+    modal.classList.remove('hidden');
 
-// Date arrays
-const dayStrings = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const monthStrings = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-let dateModifier = '';
-if(numericalDate === 1 || numericalDate === 21 || numericalDate === 31) {
-    dateModifier = 'st';
-} else if (numericalDate === 2 || numericalDate === 22) {
-    dateModifier = 'nd';
-} else if (numericalDate === 3 || numericalDate === 23) {
-    dateModifier = 'rd';
-} else {
-    dateModifier = 'th';
-}
-
-// 
-// EVENT LISTENER FUNCTION
-formElement.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Get the users typed name
-    const userName = nameInput.value;
-    console.log(userName);
-
-    //Create the flex Element and add Classes
-    const commentBoxItem = document.createElement('div');
-    commentBoxItem.classList.add('flex');
-    commentBoxItem.classList.add('comment-box');
-
-    // Create a div element to hold the IMAGE (appends to the commentBoxItem)
-    const commentBoxImg = document.createElement('div');
-    commentBoxImg.classList.add('comment-box-img');
-    commentBoxImg.innerHTML = `<img src="https://www.placecage.com/c/100/100" alt="user image">`;
-
-    // create div to hold DATE (h4) and TEXTAREA INPUT 
-    const commentBoxContainer = document.createElement('div');
-    commentBoxContainer.classList.add('comment-box-text');
-        // create date variable and h4 to hold date
-
-        const dateHeading = document.createElement('h4');
-        dateHeading.textContent = `${dayStrings[currentDay]} ${monthStrings[currentMonth]} ${numericalDate}${dateModifier}, ${currentYear} by ${userName}`;
-        // create paragraph to hold input from textarea
-
-        const commentBoxPara = document.createElement('p');
-        commentBoxPara.innerHTML = textInput.value;
-
-    // APPEND everything to the appropriate parent like in the HTML 
-    parentContainer.appendChild(commentBoxItem);
-    commentBoxItem.appendChild(commentBoxImg);
-    commentBoxItem.appendChild(commentBoxContainer);
-    commentBoxContainer.appendChild(dateHeading);
-    commentBoxContainer.appendChild(commentBoxPara);
-
-
-    // Reset conditions
-    nameInput.value = '';
-    textInput.value = '';
 })
 
+modalCloseBtn.addEventListener('click', function () {
+    // Add the hidden class, CLOSES the modal window
+    modal.classList.add('hidden');
+})
